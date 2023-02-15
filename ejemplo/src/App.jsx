ac3730d293e017545ae68dac60a8c2b7497ac697
Rdurbano <rdurbano@outlook.com>
2023-02-14 21:02:47 -0300

    prueba

---
+++ ejemplo/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+  
+   <div>
+    <Navbar/>
+    <h1> mi app</h1>
+   </div>
+  )
+}
+
+export default App
---
+++ ejemplo/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+
+import styles from './navbar.modules.css';
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+        <p>Home</p>
+        <p>About</p>
+        <p>Projects</p>
+    </nav>
+  )
+}
+
+export default Navbar
